b43fb31cb63bbe1e2682e1eb098ce245d20195cb
JavadJan <92234079+JavadJan@users.noreply.github.com>
2022-07-15 18:36:10 +0300

    javad added firebase V9 connection and react

---
+++ consupmtion/src/firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDqW3wPCT3DWfTjUbnyCCdiesaH0DQRgOg",
+    authDomain: "consumption-f2c32.firebaseapp.com",
+    projectId: "consumption-f2c32",
+    storageBucket: "consumption-f2c32.appspot.com",
+    messagingSenderId: "1095180772152",
+    appId: "1:1095180772152:web:1a38dfa85b6be3213b57cd",
+    measurementId: "G-DW4NTN6X7Y"
+  };
+  const app = initializeApp(firebaseConfig);
+
